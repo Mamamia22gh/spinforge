c3d96e24f8d4c80c32c99e6103a5dc925e98979e
eaeae <aea@eaea.com>
2026-04-13 15:33:06 +0200

    fix: title/subtitle moved to top, removed stats text from menu

--- client/main.js
+++ client/main.js
@@ -338,12 +338,8 @@ class App {
   _drawTitle(ctx) {
     this._drawDim(ctx);
 
-    drawTextCentered(ctx, 'SPINFORGE', W / 2, 50, PAL.gold, 3);
-    drawTextCentered(ctx, 'MYSTIC ROULETTE ROGUELIKE', W / 2, 78, PAL.midGray, 1);
-
-    const meta = this.game.getMeta();
-    drawTextCentered(ctx, 'STARS ' + meta.totalStars, W / 2, 160, PAL.gold, 1);
-    drawTextCentered(ctx, 'BEST ROUND ' + meta.bestRound, W / 2, 172, PAL.midGray, 1);
+    drawTextCentered(ctx, 'SPINFORGE', W / 2, 14, PAL.gold, 3);
+    drawTextCentered(ctx, 'MYSTIC ROULETTE ROGUELIKE', W / 2, 42, PAL.midGray, 1);
   }
 
   // ── HUD ──
