ca14edda74bbdc4d3e1c5d83b6600e90bd89ab60
eaeae <aea@eaea.com>
2026-04-13 17:37:17 +0200

    fix: flip Y in vertex shader (WebGL vs Canvas 2D coordinate mismatch)

--- client/gfx/PostFXGL.js
+++ client/gfx/PostFXGL.js
@@ -10,7 +10,7 @@ const VERT = `
 attribute vec2 aPos;
 varying vec2 vUv;
 void main() {
-  vUv = aPos * 0.5 + 0.5;
+  vUv = vec2(aPos.x * 0.5 + 0.5, 0.5 - aPos.y * 0.5);
   gl_Position = vec4(aPos, 0.0, 1.0);
 }`;
 
